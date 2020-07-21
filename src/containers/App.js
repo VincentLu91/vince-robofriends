b3b3b077c5097f6919dad6a038f7df0a4cc69e0d
Vince Lu <vincentlu@hotmail.ca>
2020-07-20 21:41:52 -0400

    async attempt #1

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,18 +6,22 @@ import {robots} from '../robots';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
 	return {
-		//searchField: state.searchRobots.searchField // this line is useless
-		searchField: state.searchField
+		searchField: state.searchRobots.searchField, // this line is useless unless we work with combined root reducer
+		// searchField: state.searchField // only works when searchRobots is the only reducer
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
 	}
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => requestRobots(dispatch)
 	}
 }
 
@@ -25,15 +29,18 @@ class App extends Component {
 	constructor() {
 		super()
 		this.state = { // state describes our app.
-			robots: [],
+			robots: []
 			//searchfield: ''
 		}
 	}
 	
 	componentDidMount() {
+		
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then(response => response.json())
 			.then(users => this.setState({ robots: users}));
+		
+		//this.props.onRequestRobots(); // this is supposed to work but it's pending forever
 	}
 	/*
 	onSearchChange = (event) => {
