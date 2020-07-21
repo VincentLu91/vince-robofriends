9db046aeedfce2001ece3cbca733d204e2b13f79
Vince Lu <vincentlu@hotmail.ca>
2020-07-20 21:42:08 -0400

    Updates

--- precache-manifest.2e89b2fbc37dde6253b2f3d82e931f35.js
+++ precache-manifest.2e89b2fbc37dde6253b2f3d82e931f35.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "61f1e35346d464c824437f12f90a1514",
+    "revision": "e16f91b7653b69ec42c6d5e749c95e6e",
     "url": "/vince-robofriends/index.html"
   },
   {
-    "revision": "3895c24c406d783ae7ae",
+    "revision": "60523dabb791fc68cf8d",
     "url": "/vince-robofriends/static/css/2.3c03f26f.chunk.css"
   },
   {
-    "revision": "96c5746672b7257cdee9",
+    "revision": "fbdd433a08feced3d1b2",
     "url": "/vince-robofriends/static/css/main.801fc96e.chunk.css"
   },
   {
-    "revision": "3895c24c406d783ae7ae",
-    "url": "/vince-robofriends/static/js/2.5fe570a1.chunk.js"
+    "revision": "60523dabb791fc68cf8d",
+    "url": "/vince-robofriends/static/js/2.839f4ecd.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/vince-robofriends/static/js/2.5fe570a1.chunk.js.LICENSE.txt"
+    "url": "/vince-robofriends/static/js/2.839f4ecd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "96c5746672b7257cdee9",
-    "url": "/vince-robofriends/static/js/main.b0d2885d.chunk.js"
+    "revision": "fbdd433a08feced3d1b2",
+    "url": "/vince-robofriends/static/js/main.bdc089db.chunk.js"
   },
   {
     "revision": "42cb3f989a36a9201367",
