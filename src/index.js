c0982e0733ca8c3d8b728afe036398082c96c6dc
Vince Lu <vincentlu@hotmail.ca>
2020-08-05 15:09:15 -0400

    August 5 index.js fixes 2

--- src/index.js
+++ src/index.js
@@ -19,14 +19,11 @@ const rootReducers = combineReducers({requestRobots, searchRobots})
 const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
-  <React.StrictMode>
-	<Provider store={store}>
-    	<App />
-	</Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
