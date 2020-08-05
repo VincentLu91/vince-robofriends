9f983ed70ae8fcae49266a126192cc9b09b491a5
Vince Lu <vincentlu@hotmail.ca>
2020-08-05 15:05:04 -0400

    August 5 index.js fixes 1

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import {createStore, applyMiddleware, combineReducers} from 'redux';
-import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
-import './index.css';
-import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
-import { searchRobots, requestRobots } from './reducers';
+import { createLogger } from 'redux-logger';
 import 'tachyons';
 
-const logger = createLogger();
-const rootReducer = combineReducers({ searchRobots, requestRobots });
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+import App from './containers/App';
+import * as serviceWorker from './serviceWorker';
+import { requestRobots, searchRobots } from './reducers'
+
+import './index.css';
+
+const logger = createLogger()
+
+const rootReducers = combineReducers({requestRobots, searchRobots})
+
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
   <React.StrictMode>
