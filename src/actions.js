fdf8636b743058a33aca793c40305a3a29c630ec
Vince Lu <vincentlu@hotmail.ca>
2020-08-05 15:47:17 -0400

    August 5 attempting linting fix actions.js 4

--- src/actions.js
+++ src/actions.js
@@ -3,12 +3,15 @@ import {
 	REQUEST_ROBOTS_PENDING,
 	REQUEST_ROBOTS_SUCCESS,
 	REQUEST_ROBOTS_FAILED 
-} from './constants'
+} from './constants.js';
 
-export const setSearchField = (text) => ({ type: CHANGE_SEARCHFIELD, payload: text })
+export const setSearchField = (text) => ({
+	type: CHANGE_SEARCH_FIELD,
+	payload: text
+})
 
 export const requestRobots = () => (dispatch) => {
-	dispatch({ type: REQUEST_ROBOTS_PENDING });
+	dispatch({ type: REQUEST_ROBOTS_PENDING});
 	fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response => response.json())
 		.then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
